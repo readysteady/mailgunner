9474ffc3c0a863317528fcca409969c9d4a94467
Mark Oude Veldhuis <mark.oudeveldhuis@nedap.com>
2018-08-15 22:54:56 +0200

    Updated specs to make sure EU region is properly configured

--- spec/mailgunner_delivery_method_spec.rb
+++ spec/mailgunner_delivery_method_spec.rb
@@ -18,20 +18,45 @@ describe 'Mailgunner::DeliveryMethod' do
 
     @domain = 'samples.mailgun.org'
 
-    @base_url = 'https://api.mailgun.net/v3'
-
     @auth = ['api', @api_key]
 
     @address = 'user@example.com'
 
     ActionMailer::Base.delivery_method = :mailgun
-
-    ActionMailer::Base.mailgun_settings = {api_key: @api_key, domain: @domain}
   end
 
-  it 'delivers the mail to mailgun in mime format' do
-    stub_request(:post, "#@base_url/#@domain/messages.mime").with(basic_auth: @auth)
+  describe 'for the us region' do
+    before do
+      @base_url = 'https://api.mailgun.net/v3'
 
-    ExampleMailer.registration_confirmation(email: @address).deliver_now
+      ActionMailer::Base.mailgun_settings = {
+        api_key: @api_key,
+        domain: @domain
+      }
+    end
+
+    it 'delivers the mail to mailgun in mime format' do
+      stub_request(:post, "#@base_url/#@domain/messages.mime").with(basic_auth: @auth)
+
+      ExampleMailer.registration_confirmation(email: @address).deliver_now
+    end
+  end
+
+  describe 'for the eu region' do
+    before do
+      @base_url = 'https://api.eu.mailgun.net/v3'
+
+      ActionMailer::Base.mailgun_settings = {
+        api_key: @api_key,
+        domain: @domain,
+        region: 'eu'
+      }
+    end
+
+    it 'delivers the mail to mailgun in mime format' do
+      stub_request(:post, "#@base_url/#@domain/messages.mime").with(basic_auth: @auth)
+
+      ExampleMailer.registration_confirmation(email: @address).deliver_now
+    end
   end
 end
