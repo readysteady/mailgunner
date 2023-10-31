b1e46a8f5b91f56af10cfdf2e1b3aa4ee0134168
Rainer Borene <rainerborene@gmail.com>
2023-10-31 12:21:49 -0300

    fix(rails): register delivery method before ActionMailer initialization

--- lib/mailgunner/railtie.rb
+++ lib/mailgunner/railtie.rb
@@ -1,7 +1,7 @@
 module Mailgunner
   # @private
   class Railtie < Rails::Railtie
-    config.before_initialize do
+    initializer 'mailgunner.configure_rails_initialization', before: 'action_mailer.set_configs' do
       require 'mailgunner/delivery_method'
     end
   end
